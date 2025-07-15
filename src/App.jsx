6856ed31041e30c9821e50fc28ff216b97eb359f
RomanG <gromniki@gmail.com>
2025-07-15 14:22:59 +0300

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css';
+import Button from './components/Button/Button';
+import JournalItem from './components/JournalItem/JournalItem';
+
+function App() {
+  const data = [
+		{
+			title: 'Подготовка к обновлению курсов',
+			text: 'Горные походы открывают удивительные природные ландшафты',
+			date: new Date()
+		},
+		{
+			title: 'Поход в годы',
+			text: 'Думал, что очень много времени',
+			date: new Date()
+		}
+	];
+
+  return (
+    <>
+      <h1>Курс по React от Ларичева</h1>
+      <p>Вспомогательный текст</p>
+      <Button/>
+      <JournalItem
+        title={data[0].title}
+        text={data[0].text}
+        date={data[0].date}
+      />
+      <JournalItem
+        title={data[1].title}
+        text={data[1].text}
+        date={data[1].date}
+      />
+    </>
+  );
+}
+
+export default App;
